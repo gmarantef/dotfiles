210fdc37768a88a7f1442782c4f01b0d0ba43316
Guillermo Marante <gmarantef@hotmail.com>
2026-02-20 10:47:39 +0100

    Update run_once_after_99-install-brew-packages

--- run_once_after_99-install-brew-packages.sh
+++ run_once_after_99-install-brew-packages.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 # vim: ft=sh
+set -e
+
+# Sudo at start
+echo "request sudo permissions..."
+sudo -v
+
+# Keep alive sudo
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install flatpak for packages inside brew bundle
+if ! command -v flatpak >/dev/null 2>&1: then
+    echo "installing fltapak ..."
+    sudo apt update
+    sudo apt install -y flatpak
+fi
+
+# Add flathub repository
+if ! flatpak remote-list | grep -q flathub; then
+  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
 
 # Install Homebrew packages if brew is available
 if command -v brew >/dev/null 2>&1; then
-    echo "› installing Homebrew packages..."
+    echo "installing Homebrew packages..."
     brew bundle --file="$HOME/.Brewfile"
 fi
